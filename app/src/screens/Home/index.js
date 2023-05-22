5a0238e14e146c217b9c4e919bbcb58b60a41d8e
Jean5027 <jeanviana5027@gmail.com>
2023-05-22 08:31:47 -0300

    ultimo update

--- app/src/screens/Home/index.js
+++ app/src/screens/Home/index.js
@@ -1,16 +1,41 @@
-import { View, Text } from "react-native";
+import { View, ActivityIndicator, useWindowDimensions} from "react-native";
+import { useCallback, useState } from "react";
 import YoutubeIframe from 'react-native-youtube-iframe';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
-import { VIDEO_HEIGHT, styles } from "./styles";
+import { VIDEO_HEIGHT, styles, SCREEN_SPACE } from "./styles";
+
 
 export function Home(){
+
+    const [videoready, setVideoReady] = useState(false);
+
+    const { width } = useWindowDimensions();
+    const VIDEO_WIDTH = width - (SCREEN_SPACE * 2);
+
+    const onFullScreenChange = useCallback((isFullScreen) => {
+        if (isFullScreen){
+            ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+        }else{
+            ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+        }
+    },[]);
+
     return(
         <View style={styles.container}>
-            <YoutubeIframe
-            videoId="K_8yRH2KPVo"
-            height={VIDEO_HEIGHT}
 
-            />
+            <View style={styles.player}>
+                <YoutubeIframe
+                videoId="K_8yRH2KPVo"
+                width={VIDEO_WIDTH}
+                height={videoready ? VIDEO_HEIGHT : 0}
+                onReady={() => setVideoReady(true)}
+                onFullScreenChange={onFullScreenChange}
+                />
+
+                {!videoready && <ActivityIndicator color="red"/>}
+            </View>
+
         </View>
     )
 }
--- app/src/screens/Home/styles.js
+++ app/src/screens/Home/styles.js
@@ -1,13 +1,22 @@
 import { StyleSheet } from "react-native";
 
 export const VIDEO_HEIGHT = 180
+export const SCREEN_SPACE = 24
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1 ,
-        backgroudColor: '#000',
-        padding: 24,
+        backgroudColor: '#fff',
+        padding: SCREEN_SPACE,
 
+    
+    },
+    player: {
+        width: '100%',
+        height: VIDEO_HEIGHT,
+        alignItems: 'center',
+        justifyContent: 'center',
 
     }
 })
+
